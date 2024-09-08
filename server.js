5a0e1227bb4779001eeaa1d6e725ac39846f85ce
Ramazan Shira <ramazanshira1@gmail.com>
2024-09-08 16:38:11 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors({ origin: "*" }));
+
+app.use(bodyParser.json());
+
+app.post("/send-email", async (req, res) => {
+  console.log(req.body);
+  const { name, email, message } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "ramazanshira1@gmail.com",
+      pass: "lngt pfxj qfdf qsnt",
+    },
+  });
+
+  let mailOptions = {
+    from: email,
+    to: "ramazanshira1@gmail.com",
+    subject: `New Contact Form Submission from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+    res.status(200).json({ message: "Email sent successfully!" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ message: "Error sending email", error });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
